ae8e04ea1556b35b75268a9f91dbd7a3e2dde435
MateoBot496 <mateomontesacosta@gmail.com>
2025-02-12 15:37:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 document.getElementById('change-color-btn').addEventListener('click', function() {
-    const messages = [
-        "¡Tu amor ilumina mi vida!",
-        "Eres lo mejor que me ha pasado, te amo.",
-        "Siempre estaré aquí para ti, Sofía.",
-        "Gracias por ser la mejor compañera de vida.",
-        "Te deseo todo lo mejor, hoy y siempre."
-    ];
+    // Mostrar el formulario de deseos
+    document.getElementById('wish-form').style.display = 'block';
     
-    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-    
-    document.getElementById('wish-message').textContent = randomMessage;
+    // Opcional: Ocultar el mensaje de "Haz un deseo"
 });
